e1f37127fc79445963a9fa3220550400d8e841e6
Leandro Carvalho <leandro7792@gmail.com>
2019-12-17 13:57:01 -0300

    fix bug

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ import { search } from 'sinesp-api';
 const server = express();
 server.use(express.json());
 
-server.use(function (req, res, next) {
-
-  next();
-});
-
 server.get('/:plate', async (req, res) => {
   try {
     const { plate } = req.params;
